ad8e5c407e1288022cfe92ad767d28b433f9194e
Brandon Zirulnikoff <zirbrandonweb@gmail.com>
2021-02-19 14:01:01 -0800

    Updates

---
+++ precache-manifest.51b42132dce28a0a1486b47c0b57c2f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d89bbd3650118e5d61f7c63b4931f4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "176119b3b28f1194ad6d",
+    "url": "/static/css/main.73f103ff.chunk.css"
+  },
+  {
+    "revision": "1354109f7dde3b8b7df1",
+    "url": "/static/js/2.2cc1b66c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2cc1b66c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "176119b3b28f1194ad6d",
+    "url": "/static/js/main.f6d74d75.chunk.js"
+  },
+  {
+    "revision": "e59711197170c6140a72",
+    "url": "/static/js/runtime-main.02638610.js"
+  },
+  {
+    "revision": "45697063bfc543d7b6f7ce1b0e805108",
+    "url": "/static/media/kaboom.45697063.mp3"
+  },
+  {
+    "revision": "1e61fa5564c1174988ba96c598e14fde",
+    "url": "/static/media/sploosh.1e61fa55.mp3"
+  },
+  {
+    "revision": "f1055bbd8fe4a714139157041d6c58bd",
+    "url": "/static/media/submarine.f1055bbd.jpg"
+  },
+  {
+    "revision": "344b5d23239917a8079a6c4585333486",
+    "url": "/static/media/victory.344b5d23.mp3"
+  }
+]);
